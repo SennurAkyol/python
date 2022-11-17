45432e72831e3f31f2d08f01f483087d2a3628cd
SennurAkyol <sennurakyol95@gmail.com>
2022-11-18 00:28:54 +0300

    first

---
+++ main.py
@@ -0,0 +1,44 @@
+print('Merhaba Etiya')
+# string = metinsel ifade
+text = '10'
+print(text)
+# integer =tam sayı
+number =10
+print(number)
+#double, float, decimal = ondalıklı sayı
+dnumber=5.3
+print(dnumber)
+#!matematiksel operatörler
+print(number+5)
+print(number * 5)
+print(number % 5)
+print(number%3)  # 3 e bölümünden kalan
+#boolean,bool = true, false
+isverified = True
+
+#mantıksal operatörler = her mantıksal operatör boolean değerdir.
+print(1==1)
+print(1!=2)
+#mantıksal ope = karşılaştırma
+print(2<1)
+print(2>1)
+
+print(10%2==0)
+print(50/2==25)
+print(50/2==25.0)
+
+#string operasyonları
+text='Merhaba Etiya'
+print(text.upper())
+print(text.lower())
+print(text.startswith('t'))
+print(text.endswith('etiya'))
+
+name='halit'
+age='23'
+company='kodlama'
+#halit 23 yaşında kodlamada çalışıyor
+print(name+''+age+'yaşında'+company+'de çalışıyor')
+
+print(f'{name}{age}yaşında{company}de çalışıyor')
+
