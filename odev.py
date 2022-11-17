1fe8053f1653d70696d7cceb18c8a66543cf9cde
SennurAkyol <sennurakyol95@gmail.com>
2022-11-17 11:57:02 +0300

    first

---
+++ odev.py
@@ -0,0 +1,24 @@
+baslik = 'Haberiniz Olsun' #string
+vade=12 #integer
+faizOrani1=1.47 #float
+faizOrani2=1.44
+
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+print(type(faizOrani2))
+
+mesaj= 'Hoşgeldin'
+müsteriAdi= 'Şennur'
+müsteriSoyadi= 'Akyol'
+sonucMesaj=mesaj+' '+müsteriAdi+' '+müsteriSoyadi
+print(mesaj+müsteriAdi+müsteriSoyadi)
+print(mesaj+' '+müsteriAdi+' '+müsteriSoyadi)
+print(sonucMesaj)
+
+
+sayi1=10
+sayi2=20
+print(sayi1+sayi2)
